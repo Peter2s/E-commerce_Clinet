8892475ff7773d20e37e6b4336fe9bcd0e1d8f8f
peter salah <petersalahsamir@gmail.com>
2023-06-14 00:05:34 +0300

    add Home page categories

--- src/Website/Pages/HomePage/HomePage.js
+++ src/Website/Pages/HomePage/HomePage.js
@@ -1,12 +1,14 @@
 import FooterSite from "../../components/Footer/FooterSite";
 import NavBar from "../../components/NavBar/NavBar";
 import Silder from "../../components/Home/Silder";
+import { HomeCategories } from "../../components/Home/HomeCategories";
 
 const Home = () => {
   return (
     <div id="home" dir="rtl">
       <NavBar />
       <Silder />
+      <HomeCategories />
       <FooterSite />
     </div>
   );
---
+++ src/Website/components/Home/HomeCategories.jsx
@@ -0,0 +1,47 @@
+import { HomeCategoriesHook } from "../../hook/Categories/HomeCategoriesHook";
+import { Container, Row, Spinner } from "reactstrap";
+import { CategoryCard } from "../../Pages/Categories/CategoryCard";
+import { SubTiltle } from "../Uitily/SubTiltle";
+import { useEffect } from "react";
+
+export const HomeCategories = () => {
+  const [categories, loading] = HomeCategoriesHook();
+  useEffect(() => {
+    // Handle category data after it has been fetched
+    if (categories.length > 0) {
+      console.log("useeffect", categories, loading); // Or perform any other operations with the categories data
+    }
+  }, [categories]);
+
+  return (
+    <>
+      <Container>
+        <SubTiltle
+          title="التصنيفات"
+          btntitle="المزيد"
+          pathText="/allcategory"
+        />
+        <Row className="my-2 d-flex justify-content-between">
+          {loading === false ? (
+            categories ? (
+              categories.slice(0, 5).map((item, index) => {
+                return (
+                  <CategoryCard
+                    key={index}
+                    id={item._id}
+                    title={item.name_ar}
+                    img={item.image}
+                  />
+                );
+              })
+            ) : (
+              <h4>لا يوجد تصنيفات</h4>
+            )
+          ) : (
+            <Spinner animation="border" variant="primary" />
+          )}
+        </Row>
+      </Container>
+    </>
+  );
+};
