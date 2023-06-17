12ce9b2af1f9a73066406bcb3bb94b1ba81d3d4a
nader mohammed <nader.mohammed.20x@gmail.com>
2023-06-17 04:41:58 +0300

    minor changes

--- src/Dashboard/Pages/Employees/EmpListing/EmpListing.js
+++ src/Dashboard/Pages/Employees/EmpListing/EmpListing.js
@@ -1,14 +1,14 @@
 import Tables from '../../../SharedUI/Table/Tables';
 import Btn from './../../../SharedUI/Btn/Btn';
-import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import React, {useEffect, useState} from "react";
+import {Link} from "react-router-dom";
 import Swal from 'sweetalert2';
-import { axiosInstance } from '../../../../Axios';
+import {axiosInstance} from '../../../../Axios';
 import PaginationAdmin from "../../../SharedUI/PaginationAdmin/PaginationAdmin";
 
 const Emps = () => {
-  const [empData, setEmpData] = useState([]);
-  const [total, setTotal] = useState(0);
+    const [empData, setEmpData] = useState([]);
+    const [total, setTotal] = useState(0);
     const [pagination, setPagination] = useState({
         currentPage: null,
         totalPages: null,
@@ -20,7 +20,7 @@ const Emps = () => {
             .get(`/api/v1/employees?page=${page}`)
             .then((response) => {
                 console.log(response.data);
-                const { data, pagination } = response.data;
+                const {data, pagination} = response.data;
                 setEmpData(data);
                 setTotal(pagination.total);
                 setPagination({
@@ -35,33 +35,33 @@ const Emps = () => {
     }
 
 
-  useEffect(() => {
-    fetchEmployees();
-  }, []);
-  
-  const handleDelete = async (id) => {
-    try {
-      const result = await Swal.fire({
-        title: 'Are you sure?',
-        text: 'You will not be able to recover this employee!',
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#d33',
-        cancelButtonColor: '#3085d6',
-        confirmButtonText: 'Yes, delete it!',
-        cancelButtonText: 'Cancel'
-      });
+    useEffect(() => {
+        fetchEmployees();
+    }, []);
 
-      if (result.isConfirmed) {
-        const response = await axiosInstance.delete(`/api/v1/employees/${id}`);
-        setEmpData((prevUser) =>prevUser.filter((employee) => employee.id !== id));
-        console.log(response.data);
-        Swal.fire('Deleted!', 'The role has been deleted.', 'success');
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
+    const handleDelete = async (id) => {
+        try {
+            const result = await Swal.fire({
+                title: 'Are you sure?',
+                text: 'You will not be able to recover this employee!',
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#d33',
+                cancelButtonColor: '#3085d6',
+                confirmButtonText: 'Yes, delete it!',
+                cancelButtonText: 'Cancel'
+            });
+
+            if (result.isConfirmed) {
+                const response = await axiosInstance.delete(`/api/v1/employees/${id}`);
+                setEmpData((prevUser) => prevUser.filter((employee) => employee.id !== id));
+                console.log(response.data);
+                Swal.fire('Deleted!', 'The role has been deleted.', 'success');
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
     const handleActivate = async (userId) => {
         await axiosInstance
             .post(`/api/v1/employees/${userId}/ban`)
@@ -72,7 +72,7 @@ const Emps = () => {
                     const userIndex = updatedData.findIndex(user => user.id === userId);
                     // console.log(userIndex)
                     if (userIndex !== -1) {
-                        updatedData[userIndex] = { ...updatedData[userIndex], is_banned: true };
+                        updatedData[userIndex] = {...updatedData[userIndex], is_banned: true};
                     }
                     return updatedData;
                 });
@@ -93,7 +93,7 @@ const Emps = () => {
                     const userIndex = updatedData.findIndex(user => user.id === userId);
                     // console.log(userIndex)
                     if (userIndex !== -1) {
-                        updatedData[userIndex] = { ...updatedData[userIndex], is_banned: false };
+                        updatedData[userIndex] = {...updatedData[userIndex], is_banned: false};
                     }
                     return updatedData;
                 });
@@ -105,77 +105,80 @@ const Emps = () => {
     const handlePageChange = (page) => {
         fetchEmployees(page);
     };
-  
-  return (
-    <>
-      <Tables
-          title={`Employees (${total})`}
 
-          btn={<>
-          <Link to="/admin/employees/create" className='d-flex'>
-                          <Btn
+    return (
+        <>
+            <Tables
+                title={`Employees (${total})`}
+
+                btn={<>
+                    <Link to="/admin/employees/create" className='d-flex'>
+                        <Btn
                             className="btn btn-primary ml-auto"
                             title="Add Employee"
-                          />
-                        </Link>
-          </>}
+                        />
+                    </Link>
+                </>}
 
-        trContent={
-          <>
-            {/*<th scope="col">ID</th>*/}
-            <th scope="col">#</th>
-            <th scope="col">Name</th>
-            <th scope="col">Email</th>
-            <th scope="col">Phone</th>
-            <th scope="col">Role</th>
-            <th scope="col">Actions</th>
-            <th scope="col" />
-          </>
-        }
-        tableContent={empData.map((employee, index) => (
-          <tr key={employee.id}>
-            {/*<td>{employee.id}</td>*/}
+                trContent={
+                    <>
+                        {/*<th scope="col">ID</th>*/}
+                        <th scope="col">#</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Phone</th>
+                        <th scope="col">Role</th>
+                        <th scope="col">Actions</th>
+                        <th scope="col"/>
+                    </>
+                }
+                tableContent={empData.map((employee, index) => (
+                    <tr key={employee.id}>
+                        {/*<td>{employee.id}</td>*/}
 
-            <td>{(index+1)+(pagination.currentPage-1)*pagination.limit}</td>
-            <td>{employee.name}</td>
-            <td>{employee.email}</td>
-            <td>{employee.phone}</td>
-            <td>{employee.role}</td>
-            
-                <td>
-                    {employee.is_banned ? (
-                        <Btn className="btn-danger btn fa fa-lock" onClick={() => handleDeactivate(employee.id)}/>
-                    ) : (
-                        <Btn className="btn-success btn fa fa-lock-open" onClick={() => handleActivate(employee.id)}/>
-                    )}
-                <Link to={`/admin/EmpEdit/${employee.id}`}>
-                  <Btn className="btn-primary btn fa fa-edit" />
+                        <td>{(index + 1) + (pagination.currentPage - 1) * pagination.limit}</td>
+                        <td>{employee.name?.length > 25 ? employee.name.substring(0, 25) + '...' : employee.name}</td>
+                        <td>{employee.email}</td>
+                        <td>{employee.phone}</td>
+                        <td>{employee.role}</td>
+
+                        <td>
+                    <span className="mr-2">
+                <Link to={`/admin/EmpDetails/${employee.id}`}>
+                  <Btn className="btn-info btn fa fa-circle-info"/>
                 </Link>
-                
-                <span className="ml-2">
+                </span>
+                            {employee.is_banned ? (
+                                <Btn className="btn-danger btn fa fa-lock"
+                                     onClick={() => handleDeactivate(employee.id)}/>
+                            ) : (
+                                <Btn className="btn-success btn fa fa-lock-open"
+                                     onClick={() => handleActivate(employee.id)}/>
+                            )}
+                            <Link to={`/admin/EmpEdit/${employee.id}`}>
+                                <Btn className="btn-primary btn fa fa-edit"/>
+                            </Link>
+
+                            <span className="ml-2">
                   <Btn
-                    className="btn btn-danger fa fa-trash"
-                    onClick={() => handleDelete(employee.id)}
+                      className="btn btn-danger fa fa-trash"
+                      onClick={() => handleDelete(employee.id)}
                   />
                 </span>
-                <span className="ml-2">
-                <Link to={`/admin/EmpDetails/${employee.id}`}>
-                  <Btn className="btn-info btn fa fa-circle-info" />
-                </Link>
-                </span>
-            </td>
-          </tr>
-        ))}
-          pagination={
-              <PaginationAdmin
-                  currentPage={pagination.currentPage}
-                  totalPages={pagination.totalPages}
-                  onPageChange={handlePageChange}
-              />
-          }
-      />
-    </>
-  );
+
+                        </td>
+                    </tr>
+                ))}
+                pagination={
+                    <PaginationAdmin
+                        currentPage={pagination.currentPage}
+                        totalPages={pagination.totalPages}
+                        onPageChange={handlePageChange}
+                    />
+                }
+            />
+        </>
+    );
 };
 
 export default Emps;
--- src/Dashboard/Pages/Users/UserListing/UserListing.js
+++ src/Dashboard/Pages/Users/UserListing/UserListing.js
@@ -1,182 +1,184 @@
 import Tables from '../../../SharedUI/Table/Tables';
 import Btn from 'Dashboard/SharedUI/Btn/Btn';
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import { Link } from "react-router-dom";
+import React, {useEffect, useState} from "react";
+import {Link} from "react-router-dom";
 import Swal from 'sweetalert2';
-import { axiosInstance } from '../../../../Axios';
+import {axiosInstance} from '../../../../Axios';
 import PaginationAdmin from "../../../SharedUI/PaginationAdmin/PaginationAdmin";
 
 
 const Users = () => {
-  const [userData, setUserData] = useState([]);
-  const [total, setTotal] = useState(0);
+    const [userData, setUserData] = useState([]);
+    const [total, setTotal] = useState(0);
     const [pagination, setPagination] = useState({
         currentPage: null,
         totalPages: null,
         limit: null,
     });
 
-  useEffect(() => {
-    fetchUsers();
-    
-  },[]);
+    useEffect(() => {
+        fetchUsers();
 
-  const fetchUsers = async (page=1) => {
-    await axiosInstance
-      .get("/api/v1/users?page="+page)
-      .then((res) => {
-        setUserData(res.data.data);
-        setTotal(res.data.pagination.total);
-          setPagination({
-              currentPage: res.data.pagination.current_page,
-              totalPages: res.data.pagination.total_pages,
-              limit: res.data.pagination.limit,
-          });
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
-  
-  const handleDelete = async (id) => {
-    try {
-      const result = await Swal.fire({
-        title: 'Are you sure?',
-        text: 'You will not be able to recover this user!',
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#d33',
-        cancelButtonColor: '#3085d6',
-        confirmButtonText: 'Yes, delete it!',
-        cancelButtonText: 'Cancel'
-      });
+    }, []);
 
-      if (result.isConfirmed) {
-        await axiosInstance.delete(`/api/v1/users/${id}`).then((res) => {
-            console.log(res);
-            setUserData((prevUser) =>prevUser.filter((user) => user.id !== id));
-            Swal.fire('Deleted!', 'The User has been deleted.', 'success');
-        }).catch((err) => {
-            console.log(err);
-            Swal.fire('Error!', err?.response?.data?.error || 'The User can not been deleted.', 'error');
-        });
+    const fetchUsers = async (page = 1) => {
+        await axiosInstance
+            .get("/api/v1/users?page=" + page)
+            .then((res) => {
+                setUserData(res.data.data);
+                setTotal(res.data.pagination.total);
+                setPagination({
+                    currentPage: res.data.pagination.current_page,
+                    totalPages: res.data.pagination.total_pages,
+                    limit: res.data.pagination.limit,
+                });
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    };
+
+    const handleDelete = async (id) => {
+        try {
+            const result = await Swal.fire({
+                title: 'Are you sure?',
+                text: 'You will not be able to recover this user!',
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#d33',
+                cancelButtonColor: '#3085d6',
+                confirmButtonText: 'Yes, delete it!',
+                cancelButtonText: 'Cancel'
+            });
+
+            if (result.isConfirmed) {
+                await axiosInstance.delete(`/api/v1/users/${id}`).then((res) => {
+                    console.log(res);
+                    setUserData((prevUser) => prevUser.filter((user) => user.id !== id));
+                    Swal.fire('Deleted!', 'The User has been deleted.', 'success');
+                }).catch((err) => {
+                    console.log(err);
+                    Swal.fire('Error!', err?.response?.data?.error || 'The User can not been deleted.', 'error');
+                });
 
 
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  const handleActivate = async (userId) => {
-    await axiosInstance
-    .post(`/api/v1/users/${userId}/ban`)
-      .then((res) => {
-        // Update the user data
-        setUserData(prevData => {
-          const updatedData = [...prevData];
-          const userIndex = updatedData.findIndex(user => user._id === userId);
-          if (userIndex !== -1) {
-            updatedData[userIndex] = { ...updatedData[userIndex], is_active: true };
-          }
-          return updatedData;
-        });
-      })
-      
-      .catch((err) => {
-        console.log(err.message);
-      });
-  };
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
+    const handleActivate = async (userId) => {
+        await axiosInstance
+            .post(`/api/v1/users/${userId}/ban`)
+            .then((res) => {
+                // Update the user data
+                setUserData(prevData => {
+                    const updatedData = [...prevData];
+                    const userIndex = updatedData.findIndex(user => user._id === userId);
+                    if (userIndex !== -1) {
+                        updatedData[userIndex] = {...updatedData[userIndex], is_active: true};
+                    }
+                    return updatedData;
+                });
+            })
 
-  const handleDeactivate = async (userId) => {
-    await axiosInstance
-    .post(`/api/v1/users/${userId}/unban`)
-      .then((res) => {
-        // Update the user data
-        setUserData(prevData => {
-          const updatedData = [...prevData];
-          const userIndex = updatedData.findIndex(user => user._id === userId);
-          if (userIndex !== -1) {
-            updatedData[userIndex] = { ...updatedData[userIndex], is_active: false };
-          }
-          return updatedData;
-        });
-      })
-      .catch((err) => {
-        console.log(err.message);
-      });
-  };
+            .catch((err) => {
+                console.log(err.message);
+            });
+    };
 
-    const handlePageChange =  (page) => {
+    const handleDeactivate = async (userId) => {
+        await axiosInstance
+            .post(`/api/v1/users/${userId}/unban`)
+            .then((res) => {
+                // Update the user data
+                setUserData(prevData => {
+                    const updatedData = [...prevData];
+                    const userIndex = updatedData.findIndex(user => user._id === userId);
+                    if (userIndex !== -1) {
+                        updatedData[userIndex] = {...updatedData[userIndex], is_active: false};
+                    }
+                    return updatedData;
+                });
+            })
+            .catch((err) => {
+                console.log(err.message);
+            });
+    };
+
+    const handlePageChange = (page) => {
         fetchUsers(page);
     }
-  return (
-    <>
-      <Tables
-         btn={<>
-          <Link to="/admin/users/create" className='d-flex'>
-                          <Btn
+    return (
+        <>
+            <Tables
+                btn={<>
+                    <Link to="/admin/users/create" className='d-flex'>
+                        <Btn
                             className="btn btn-primary ml-auto"
                             title="Add User"
-                          />
-                        </Link>
-          </>}
-        title={`Users (${total})`}
-        trContent={
-          <>
-            {/*<th scope="col">ID</th>*/}
-            <th scope="col">#</th>
-            <th scope="col">Name</th>
-            <th scope="col">Email</th>
-            <th scope="col">Phone</th>
-            <th scope="col">Bio</th>
-            <th className="text-center" scope="col">Actions</th>
-            <th scope="col" />
-          </>
-        }
-        tableContent={userData.map((user, index) => (
-          <tr key={user.id}>
-            {/*<td>{user.id}</td>*/}
-              <td>{(index+1)+(pagination.currentPage-1)*pagination.limit}</td>
-            <td>{user.name}</td>
-            <td>{user.email}</td>
-            <td>{user.phone}</td>
-            <td>{user.bio}</td>
-            <td>
-                {user.is_active ? (
-                    <Btn className="btn-danger btn fa fa-lock" onClick={() => handleDeactivate(user.id)}/>
-                  ) : (
-                    <Btn className="btn-success btn fa fa-lock-open" onClick={() => handleActivate(user.id)}/>
-                  )}
-                <span className="ml-2"> 
-                <Link to={`/admin/UserEdit/${user.id}`}>
-                  <Btn className="btn-primary btn fa fa-edit" />
+                        />
+                    </Link>
+                </>}
+                title={`Users (${total})`}
+                trContent={
+                    <>
+                        {/*<th scope="col">ID</th>*/}
+                        <th scope="col">#</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Phone</th>
+                        <th scope="col">Bio</th>
+                        <th className="text-center" scope="col">Actions</th>
+                        <th scope="col"/>
+                    </>
+                }
+                tableContent={userData.map((user, index) => (
+                    <tr key={user.id}>
+                        {/*<td>{user.id}</td>*/}
+                        <td>{(index + 1) + (pagination.currentPage - 1) * pagination.limit}</td>
+                        <td>{user.name?.length > 25 ? user.name.substring(0, 25) + '...' : user.name}</td>
+                        <td>{user.email}</td>
+                        <td>{user.phone}</td>
+
+                        <td>{user.bio?.length > 30 ? user.bio.substring(0, 30) + '...' : user.bio}</td>
+                        <td>
+                <span className="mr-2">
+                <Link to={`/admin/UserDetail/${user.id}`}>
+                  <Btn className="btn-info btn fa fa-circle-info"/>
                 </Link>
                 </span>
-                <span className="ml-2">
+                            {user.is_active ? (
+                                <Btn className="btn-danger btn fa fa-lock" onClick={() => handleDeactivate(user.id)}/>
+                            ) : (
+                                <Btn className="btn-success btn fa fa-lock-open"
+                                     onClick={() => handleActivate(user.id)}/>
+                            )}
+
+                            <Link to={`/admin/UserEdit/${user.id}`}>
+                                <Btn className="btn-primary btn fa fa-edit"/>
+                            </Link>
+
+                            <span className="ml-2">
                   <Btn
-                    className="btn btn-danger fa fa-trash"
-                    onClick={() => handleDelete(user.id)}
+                      className="btn btn-danger fa fa-trash"
+                      onClick={() => handleDelete(user.id)}
                   />
                 </span>
-                <span className="ml-2">
-                <Link to={`/admin/UserDetail/${user.id}`}>
-                  <Btn className="btn-info btn fa fa-circle-info" />
-                </Link>
-                </span>
-            </td>
-          </tr>
-        ))}
-         pagination={
-             <PaginationAdmin
-                 currentPage={pagination.currentPage}
-                 totalPages={pagination.totalPages}
-                 onPageChange={handlePageChange}
-             />
-         }
-      />
-    </>
-  );
+
+                        </td>
+                    </tr>
+                ))}
+                pagination={
+                    <PaginationAdmin
+                        currentPage={pagination.currentPage}
+                        totalPages={pagination.totalPages}
+                        onPageChange={handlePageChange}
+                    />
+                }
+            />
+        </>
+    );
 };
 
 export default Users;
