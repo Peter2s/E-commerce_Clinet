f7ceeef18f6eb73e4646a5ed9481d98c8574c752
nader mohammed <nader.mohammed.20x@gmail.com>
2023-06-19 05:52:24 +0300

    create role done

--- src/Dashboard/Pages/Roles/CreateRole/CreateRole.js
+++ src/Dashboard/Pages/Roles/CreateRole/CreateRole.js
@@ -1,38 +1,43 @@
-import React, { useState, useEffect } from 'react';
-import { Card, CardBody, CardFooter, CardHeader, Container, Input, Row } from 'reactstrap';
-import * as Yup from 'yup';
-import axios from 'axios';
-import MySwal from 'sweetalert2';
-import Btn from 'Dashboard/SharedUI/Btn/Btn';
-import { axiosInstance } from '../../../../Axios';
-import { Navigate, useNavigate } from 'react-router';
-import { useFormik } from 'formik';
+import React, { useState, useEffect } from "react";
+import {
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Container,
+  Input,
+  Row,
+} from "reactstrap";
+import * as Yup from "yup";
+import MySwal from "sweetalert2";
+import Btn from "Dashboard/SharedUI/Btn/Btn";
+import { axiosInstance } from "../../../../Axios";
+import { Navigate, useNavigate } from "react-router";
+import { useFormik } from "formik";
+import { Checkbox } from "primereact/checkbox";
 
 const CreateRole = () => {
   const initialRole = {
-    newRole: '',
+    newRole: "",
     permissions: [],
   };
-  const methodes = ["GET", "POST", "PATCH", "DELETE","ACTIVATE","DEACTIVATE"];
+  const methodes = ["GET", "POST", "PATCH", "DELETE", "UNBAN", "BAN"];
   const [routes, setRoutes] = useState([]);
   const [permissionsData, setPermissionsData] = useState([]);
   const navigate = useNavigate();
 
   const fetchRoutes = async () => {
     try {
-      const response = await axiosInstance.get('/api/v1/routes');
-      const { routes:route } = response.data.data;
+      const response = await axiosInstance.get("/api/v1/routes");
+      const { routes: route } = response.data.data;
       setRoutes(route);
-
     } catch (error) {
       console.log(error);
     }
-  }
-
+  };
 
   useEffect(() => {
     fetchRoutes();
-
 
     // fetchPermissions();
   }, []);
@@ -46,35 +51,39 @@ const CreateRole = () => {
         setPermissionsData(permissions);
       }
     } catch (error) {
-      console.error('Error fetching permissions:', error);
+      console.error("Error fetching permissions:", error);
     }
   };
   const validationSchema = Yup.object().shape({
     newRole: Yup.string()
-      .required('Role name is required')
-      .test('unique', 'Role name must be unique', async (value) => {
+      .required("Role name is required")
+      .test("unique", "Role name must be unique", async (value) => {
         try {
-          const response = await axiosInstance.get(`/api/v1/roles?name=${value}`);
+          const response = await axiosInstance.get(
+            `/api/v1/roles?name=${value}`
+          );
           const roles = response.data.data;
           return roles.length === 0;
         } catch (error) {
-          console.error('Error checking role uniqueness:', error);
+          console.error("Error checking role uniqueness:", error);
           return false;
         }
       }),
-      permissions: Yup.array()
-      .min(1, 'At least one permission must be selected')
-      .required('At least one permission must be selected'),
+    permissions: Yup.array()
+      .min(1, "At least one permission must be selected")
+      .required("At least one permission must be selected"),
   });
 
   const formik = useFormik({
     initialValues: initialRole,
     validationSchema,
     onSubmit: async (values) => {
+      console.log("values:", values);
+
       try {
         const permissionsByEntity = {};
         values.permissions.forEach((permission) => {
-          const [entity, access] = permission.split(':');
+          const [entity, access] = permission.split(":");
           if (!permissionsByEntity[entity]) {
             permissionsByEntity[entity] = {
               entity,
@@ -83,24 +92,25 @@ const CreateRole = () => {
           }
           permissionsByEntity[entity].access[access.toLowerCase()] = true;
         });
-
+        // console.log("permissionsByEntity:", permissionsByEntity);
         const roleData = {
           name: values.newRole,
           permissions: Object.values(permissionsByEntity),
         };
-
+        // console.log("roleData:", roleData);
+        // return;
         const response = await axiosInstance.post("/api/v1/roles", roleData);
-        console.log('Role saved:', response.data);
+        // console.log("Role saved:", response.data);
 
         MySwal.fire({
-          icon: 'success',
-          title: 'Success!',
-          text: 'New Role has been added successfully.',
+          icon: "success",
+          title: "Success!",
+          text: "New Role has been added successfully.",
         });
 
         navigate("/admin/roles");
       } catch (error) {
-        console.error('Error saving role:', error);
+        console.error("Error saving role:", error);
       }
     },
   });
@@ -110,9 +120,15 @@ const CreateRole = () => {
     const isChecked = event.target.checked;
 
     if (isChecked) {
-      formik.setFieldValue('permissions', [...formik.values.permissions, permission]);
+      formik.setFieldValue("permissions", [
+        ...formik.values.permissions,
+        permission,
+      ]);
     } else {
-      formik.setFieldValue('permissions', formik.values.permissions.filter((p) => p !== permission));
+      formik.setFieldValue(
+        "permissions",
+        formik.values.permissions.filter((p) => p !== permission)
+      );
     }
   };
 
@@ -124,14 +140,15 @@ const CreateRole = () => {
             <Card className="shadow">
               <CardHeader className="border-0">
                 <h2>Add Role</h2>
-                <hr/>
+                <hr />
               </CardHeader>
               <CardBody>
                 <form className="ms-5" onSubmit={formik.handleSubmit}>
                   <label htmlFor="roleName">
-                    <b>Role Name</b><span className="required">*</span>
+                    <b>Role Name</b>
+                    <span className="required">*</span>
                   </label>
-                    <Input
+                  <Input
                     className="w-50 form-control-alternative"
                     type="text"
                     id="roleName"
@@ -147,56 +164,82 @@ const CreateRole = () => {
 
                   <br />
                   <label htmlFor="permissions">
-                    <b>Permissions</b><span className="required">*</span>
+                    <b>Permissions</b>
+                    <span className="required">*</span>
                   </label>
                   <br />
 
+                  <div>
+                    <table className="table table-bordered">
+                      <thead>
+                        <tr>
+                          <th scope="col" className="text-center">
+                            Route
+                          </th>
+                          {/*<th scope="col" className="text-center">
+                            Select All
+                          </th>*/}
+                          <th scope="col" className="text-center">
+                            View - View ALl
+                          </th>
+                          <th scope="col" className="text-center">
+                            Create
+                          </th>
+                          <th scope="col" className="text-center">
+                            Update
+                          </th>
+                          <th scope="col" className="text-center">
+                            Delete
+                          </th>
+                          <th scope="col" className="text-center">
+                            Activate
+                          </th>
+                          <th scope="col" className="text-center">
+                            Deactivate
+                          </th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {routes.map((route) => (
+                          <tr id={route}>
+                            <td className="text-center">{route}</td>
 
-                      <div  >
-                        <table className="table table-bordered">
-                            <thead>
-                                <tr>
-                                    <th scope="col" className="text-center">Route</th>
-                                    <th scope="col" className="text-center">View - View ALl</th>
-                                    <th scope="col" className="text-center">Create</th>
-                                    <th scope="col" className="text-center">Update</th>
-                                    <th scope="col" className="text-center">Delete</th>
-                                    <th scope="col" className="text-center">Activate</th>
-                                    <th scope="col" className="text-center">Deactivate</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                            {routes.map((route) => (
-                                <tr>
-                                    <td className="text-center">{route}</td>
-                                  {methodes.map((method) => (
-                                    <td className="text-center">
-                                        <div className="custom-control custom-checkbox text-center">
-                                            <input
-                                                type="checkbox"
-                                                className="custom-control-input"
-                                                id={`${route}:${method}`}
-                                                value={`${route}:${method}`}
-                                                checked={formik.values.permissions.includes(`${route}:${method}`)}
-                                                onChange={handlePermissionChange}
-                                            />
-                                            <label className="custom-control-label" htmlFor={`${route}:${method}`}>
-                                            </label>
-                                        </div>
-                                    </td>))}
-                                </tr>))}
-                            </tbody>
-                        </table>
-                        </div>
+                            {methodes.map((method) => (
+                              <td className="text-center">
+                                <Checkbox
+                                  id={`${route}:${method}`}
+                                  value={`${route}:${method}`}
+                                  checked={formik.values.permissions.includes(
+                                    `${route}:${method}`
+                                  )}
+                                  onChange={handlePermissionChange}
+                                />
+                              </td>
+                            ))}
+                          </tr>
+                        ))}
+                      </tbody>
+                    </table>
+                  </div>
 
-
-                    {formik.touched.permissions && formik.errors.permissions ? (
-                    <div className="text-danger">{formik.errors.permissions}</div>
-                    ) : null}
+                  {formik.touched.permissions && formik.errors.permissions ? (
+                    <div className="text-danger">
+                      {formik.errors.permissions}
+                    </div>
+                  ) : null}
 
                   <CardFooter>
-                    <Btn type="submit" title="Save" className="btn btn-primary" />
-                    <Btn type="button" title="Cancel" className="btn btn-light" onClick={() => navigate('/admin/roles')} />
+                    <Btn
+                      type="submit"
+                      title="Save"
+                      className="btn btn-primary"
+                    />
+                    <Btn
+                      type="button"
+                      title="Cancel"
+                      className="btn btn-light"
+                      onClick={() => navigate("/admin/roles")}
+                    />
                   </CardFooter>
                 </form>
               </CardBody>
