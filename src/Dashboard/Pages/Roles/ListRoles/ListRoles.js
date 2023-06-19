bccfbac36c506e6c89d46d78cd18eea4607df124
nader mohammed <nader.mohammed.20x@gmail.com>
2023-06-19 04:48:54 +0300

    some enhancements

--- src/Dashboard/Pages/Roles/ListRoles/ListRoles.js
+++ src/Dashboard/Pages/Roles/ListRoles/ListRoles.js
@@ -1,29 +1,29 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import Swal from 'sweetalert2';
-import { axiosInstance } from '../../../../Axios';
-import Btn from 'Dashboard/SharedUI/Btn/Btn';
-import Tables from './../../../SharedUI/Table/Tables';
-import { Link } from 'react-router-dom';
-import './ListRoles.css';
-import PaginationAdmin from './../../../SharedUI/PaginationAdmin/PaginationAdmin';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Swal from "sweetalert2";
+import { axiosInstance } from "../../../../Axios";
+import Btn from "Dashboard/SharedUI/Btn/Btn";
+import Tables from "./../../../SharedUI/Table/Tables";
+import { Link } from "react-router-dom";
+import "./ListRoles.css";
+import PaginationAdmin from "./../../../SharedUI/PaginationAdmin/PaginationAdmin";
 import DataTable from "../../../SharedUI/DataTable/DataTable";
 
 const Roles = () => {
-    const maping = {
-        "get": "View / View All",
-        "post": "Create",
-        "patch": "Edit",
-        "delete": "Delete",
-        "ban": "Ban",
-        "unban": "Unban",
-        "activate": "Activate",
-        "deactivate": "Deactivate",
-    }
-
+  const maping = {
+    get: "View / View All",
+    post: "Create",
+    patch: "Edit",
+    delete: "Delete",
+    ban: "Ban",
+    unban: "Unban",
+    activate: "Activate",
+    deactivate: "Deactivate",
+  };
 
   const [roles, setRoles] = useState([]);
   // For pagination
+  const [search, setSearch] = useState(""); // Search keyword
   const [rows, setRows] = useState(5); // Number of rows per page
   const [page, setPage] = useState(1); // Current page number
   const [loading, setLoading] = useState(true);
@@ -36,15 +36,19 @@ const Roles = () => {
   // console.log("routes",routes.map((route) => route));
   useEffect(() => {
     setLoading(true);
-    fetchRolesData();
+    fetch();
+  }, [rows, page, search]);
 
-  }, [rows, page]);
-
-
-
-  const fetchRolesData = async () => {
+  const fetch = async () => {
     try {
-      const response = await axiosInstance.get(`/api/v1/roles?page=${page}&limit=${rows}`);
+      const response = await axiosInstance.get(
+        `/api/v1/roles?page=${page}&limit=${rows}`,
+        {
+          params: {
+            keyword: search,
+          },
+        }
+      );
       const { data, pagination } = response.data;
       setRoles(data);
 
@@ -60,64 +64,73 @@ const Roles = () => {
     }
   };
 
+  const handleActivate = async (userId) => {
+    await axiosInstance
+      .post(`/api/v1/roles/${userId}/ban`)
+      .then((res) => {
+        // Update the user data
+        setRoles((prevData) => {
+          const updatedData = [...prevData];
+          const userIndex = updatedData.findIndex(
+            (user) => user._id === userId
+          );
+          // console.log(userIndex)
+          if (userIndex !== -1) {
+            updatedData[userIndex] = {
+              ...updatedData[userIndex],
+              is_active: false,
+            };
+          }
+          return updatedData;
+        });
+      })
 
-    const handleActivate = async (userId) => {
-        await axiosInstance
-            .post(`/api/v1/roles/${userId}/ban`)
-            .then((res) => {
-                // Update the user data
-                setRoles(prevData => {
-                    const updatedData = [...prevData];
-                    const userIndex = updatedData.findIndex(user => user.id === userId);
-                    // console.log(userIndex)
-                    if (userIndex !== -1) {
-                        updatedData[userIndex] = {...updatedData[userIndex], is_banned: true};
-                    }
-                    return updatedData;
-                });
-            })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
 
-            .catch((err) => {
-                console.log(err.message);
-            });
-    };
-
-    const handleDeactivate = async (userId) => {
-        await axiosInstance
-            .post(`/api/v1/roles/${userId}/unban`)
-            .then((res) => {
-                // Update the user data
-                setRoles(prevData => {
-                    const updatedData = [...prevData];
-                    const userIndex = updatedData.findIndex(user => user.id === userId);
-                    // console.log(userIndex)
-                    if (userIndex !== -1) {
-                        updatedData[userIndex] = {...updatedData[userIndex], is_banned: false};
-                    }
-                    return updatedData;
-                });
-            })
-            .catch((err) => {
-                console.log(err.message);
-            });
-    };
+  const handleDeactivate = async (userId) => {
+    await axiosInstance
+      .post(`/api/v1/roles/${userId}/unban`)
+      .then((res) => {
+        // Update the user data
+        setRoles((prevData) => {
+          const updatedData = [...prevData];
+          const userIndex = updatedData.findIndex(
+            (user) => user._id === userId
+          );
+          // console.log(userIndex)
+          if (userIndex !== -1) {
+            updatedData[userIndex] = {
+              ...updatedData[userIndex],
+              is_active: true,
+            };
+          }
+          return updatedData;
+        });
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
   const handleDelete = async (id) => {
     try {
       const result = await Swal.fire({
-        title: 'Are you sure?',
-        text: 'You will not be able to recover this role!',
-        icon: 'warning',
+        title: "Are you sure?",
+        text: "You will not be able to recover this role!",
+        icon: "warning",
         showCancelButton: true,
-        confirmButtonColor: '#d33',
-        cancelButtonColor: '#3085d6',
-        confirmButtonText: 'Yes, delete it!',
-        cancelButtonText: 'Cancel',
+        confirmButtonColor: "#d33",
+        cancelButtonColor: "#3085d6",
+        confirmButtonText: "Yes, delete it!",
+        cancelButtonText: "Cancel",
       });
 
       if (result.isConfirmed) {
         await axiosInstance.delete(`/api/v1/roles/${id}`);
         setRoles((prevRoles) => prevRoles.filter((role) => role._id !== id));
-        Swal.fire('Deleted!', 'The role has been deleted.', 'success');
+        Swal.fire("Deleted!", "The role has been deleted.", "success");
       }
     } catch (error) {
       console.log(error);
@@ -125,75 +138,77 @@ const Roles = () => {
   };
 
   const handlePageChange = async (event) => {
-
     setPage(event.page + 1);
     setRows(event.rows);
-
-  }
+  };
 
   return (
     <>
       <DataTable
-          title="Employees"
-          addBtn={
-            <Link to="/admin/roles/create" className="d-flex">
-              <Btn className="btn btn-primary ml-auto" title="Add Role" />
-            </Link>
-          }
-          data={roles}
-          loading={loading}
-          pagination={pagination}
-          onPageChange={handlePageChange}
-          columns={[
-            {header: 'Name', field: 'name',},
-              {
-                  header: 'Permissions', body: (role) => {
-
-                      return role.permissions.map((permission) => {
-                          console.log(permission)
-                          return (
-                              <div key={permission.entity}>
-                                  <strong>{permission.entity}:</strong>{' '}
-                                  {Object.entries(permission.access)
-                                      .filter(([_, value]) => value)
-                                      .map(([key, _]) => maping[key.toLowerCase()])
-                                      .join(', ')}
-                              </div>
-                          )} );
-
-                  }
-              },
-            {
-              header: 'Actions', body: (rowData) => {
+        title="Roles"
+        setSearch={setSearch}
+        addBtn={
+          <Link to="/admin/roles/create" className="d-flex">
+            <Btn className="btn btn-primary ml-auto" title="Add Role" />
+          </Link>
+        }
+        data={roles}
+        loading={loading}
+        pagination={pagination}
+        onPageChange={handlePageChange}
+        columns={[
+          { header: "Name", field: "name" },
+          {
+            header: "Permissions",
+            body: (role) => {
+              return role.permissions.map((permission) => {
+                console.log(permission);
                 return (
-                    <div className="d-flex justify-content-around">
-
-                      <Link to={""}>
-                        {!rowData.is_active ? (
-                            <Btn className="btn-danger btn fa fa-lock"
-                                 onClick={() => handleDeactivate(rowData._id)}/>
-                        ) : (
-                            <Btn className="btn-success btn fa fa-lock-open"
-                                 onClick={() => handleActivate(rowData._id)}/>
-                        )}
-                      </Link>
-                      <Link to={`/admin/roles/edit/${rowData._id}`}>
-                        <Btn className="btn-primary btn fa fa-edit"/>
-                      </Link>
-                      <Link to={""}>
-                        <Btn
-                            className="btn btn-danger fa fa-trash"
-                            onClick={() => handleDelete(rowData._id)}
-                        />
-                      </Link>
-                    </div>
+                  <div key={permission.entity}>
+                    <strong>{permission.entity}:</strong>{" "}
+                    {Object.entries(permission.access)
+                      .filter(([_, value]) => value)
+                      .map(([key, _]) => maping[key.toLowerCase()])
+                      .join(", ")}
+                  </div>
                 );
-              }
+              });
             },
-          ]}
+          },
+          {
+            header: "Actions",
+            body: (rowData) => {
+              return (
+                <div className="d-flex justify-content-around">
+                  {!rowData.is_active ? (
+                    <button
+                      title={"Activate"}
+                      className="btn-danger btn fa fa-lock"
+                      onClick={() => handleDeactivate(rowData._id)}
+                    />
+                  ) : (
+                    <button
+                      title={"Deactivate"}
+                      className="btn-success btn fa fa-lock-open"
+                      onClick={() => handleActivate(rowData._id)}
+                    />
+                  )}
 
+                  <Link to={`/admin/roles/edit/${rowData._id}`}>
+                    <Btn className="btn-primary btn fa fa-edit" />
+                  </Link>
+
+                  <button
+                    title={"Delete"}
+                    className="btn btn-danger fa fa-trash"
+                    onClick={() => handleDelete(rowData._id)}
+                  />
+                </div>
+              );
+            },
+          },
+        ]}
       />
-
     </>
   );
 };
--- src/Dashboard/SharedUI/DataTable/DataTable.js
+++ src/Dashboard/SharedUI/DataTable/DataTable.js
@@ -1,63 +1,98 @@
-import {Card, CardBody, CardFooter, CardHeader, Col, Container, Navbar, Row} from "reactstrap";
-import {DataTable} from 'primereact/datatable';
-import {Column} from 'primereact/column';
+import {
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Col,
+  Container,
+  Navbar,
+  Row,
+} from "reactstrap";
+import { DataTable } from "primereact/datatable";
+import { Column } from "primereact/column";
+import { InputText } from "primereact/inputtext";
 import React from "react";
-import "./DataTable.css"
+import "./DataTable.css";
 // core components
 
-const Tables = ({title, data, columns, addBtn, pagination, onPageChange}) => {
-    return (
-        <>
-            <Navbar/>
-            {/* Page content */}
-            <Container className="mt--7" fluid>
-                {/* Table */}
+const Tables = ({
+  title,
+  data,
+  columns,
+  addBtn,
+  pagination,
+  onPageChange,
+  setSearch,
+}) => {
+  return (
+    <>
+      <Navbar />
+      {/* Page content */}
+      <Container className="mt--7" fluid>
+        {/* Table */}
+        <Row>
+          <div className="col">
+            <Card className="shadow">
+              <CardHeader className="border-0">
                 <Row>
-                    <div className="col">
-                        <Card className="shadow">
-                            <CardHeader className="border-0">
-
-                                <Row>
-                                    <Col>
-                                        <h3 className="mb-0">{title} ({pagination.total})</h3>
-                                    </Col>
-                                    <Col>
-                                        {addBtn}
-                                    </Col>
-                                </Row>
-                            </CardHeader>
-                            <CardBody>
-                            <DataTable value={data}
-                                       dataKey="id"
-                                       paginator lazy
-                                       first={(pagination.currentPage - 1) * pagination.limit}
-                                       rows={pagination.limit}
-                                       totalRecords={pagination.total}
-                                       onPage={onPageChange}
-                                       rowsPerPageOptions={[5, 10, 25, 50]}
-                                       tableStyle={{minWidth: '50rem'}}
-                                       paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown"
-                                       currentPageReportTemplate="Showing {first} to {last} of {totalRecords} records"
-                            >
-                                <Column body={(rowData, {rowIndex}) => {
-                                    return rowIndex + 1
-                                }} header="#"></Column>
-                                {columns.map((column) => (
-                                    <Column key={column.field} field={column.field} body={column.body}
-                                            header={column.header}/>
-                                ))}
-                            </DataTable>
-                            </CardBody>
-                            <CardFooter className="py-4">
-
-                            </CardFooter>
-
-                        </Card>
-                    </div>
+                  <Col>
+                    <h3 className="mb-0">
+                      {title} ({pagination.total})
+                    </h3>
+                  </Col>
+                  {addBtn && <Col>{addBtn}</Col>}
+                  {setSearch && (
+                    <Col>
+                      <span className="p-input-icon-left">
+                        <i className="pi pi-search" />
+                        <InputText
+                          type="search"
+                          onInput={(e) => setSearch(e.target.value)}
+                          placeholder="Search by name..."
+                        />
+                      </span>
+                    </Col>
+                  )}
                 </Row>
-            </Container>
-        </>
-    );
+              </CardHeader>
+              <CardBody>
+                <DataTable
+                  value={data}
+                  dataKey="id"
+                  paginator
+                  lazy
+                  first={(pagination.currentPage - 1) * pagination.limit}
+                  rows={pagination.limit}
+                  totalRecords={pagination.total}
+                  onPage={onPageChange}
+                  rowsPerPageOptions={[5, 10, 25, 50]}
+                  tableStyle={{ minWidth: "50rem" }}
+                  paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown"
+                  currentPageReportTemplate="Showing {first} to {last} of {totalRecords} records"
+                >
+                  <Column
+                    body={(rowData, { rowIndex }) => {
+                      return rowIndex + 1;
+                    }}
+                    header="#"
+                  ></Column>
+                  {columns.map((column) => (
+                    <Column
+                      key={column.field}
+                      field={column.field}
+                      body={column.body}
+                      header={column.header}
+                    />
+                  ))}
+                </DataTable>
+              </CardBody>
+              <CardFooter className="py-4"></CardFooter>
+            </Card>
+          </div>
+        </Row>
+      </Container>
+    </>
+  );
 };
 
 export default Tables;
